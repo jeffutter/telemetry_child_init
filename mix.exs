e95e5b7db579995c109e1db0e52c1a66e7fcc321
Jeffery Utter <jeff@jeffutter.com>
2024-07-22 23:34:16 -0500

    Prep v0.1.0 release

--- mix.exs
+++ mix.exs
@@ -7,21 +7,35 @@ defmodule TelemetryChildInit.MixProject do
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "TelemetryChildInit",
+      source_url: "https://github.com/jeffutter/telemetry_child_init",
+      description: description(),
+      package: package()
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    [extra_applications: [:logger]]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
+    [{:telemetry, "~> 1.2.1"}]
+  end
+
+  defp description() do
+    "Add telemetry events to your supervisors to track child startup times."
+  end
+
+  defp package() do
     [
-      {:telemetry, "~> 1.2.1"}
+      name: "telemetry_child_init",
+      files:
+        ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE* license* CHANGELOG* changelog* src),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/jeffutter/telemetry_child_init"}
     ]
   end
 end
